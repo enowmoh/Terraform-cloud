ba5acea571559e28456718a2624fa9aac73fcf3e
Enow-moh <116763125+enowmoh@users.noreply.github.com>
2023-02-01 11:54:30 -0600

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,15 @@
+variable "ingressrules" {
+    type = list(number)
+    default = [80,443,8080]
+}
+
+variable "egressrules" {
+    type = list(number)
+    default = [80,443,25,3306,53,8080]
+}
+
+variable "my_instance_type"{
+  type = string
+  default = "t2.medium"
+}
 
