e53ab6ac89b031edff00273055fb21b53f990600
Enow-moh <116763125+enowmoh@users.noreply.github.com>
2023-02-01 19:54:11 -0600

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,12 +1,14 @@
 resource "aws_security_group" "web-traffic" {
-    name = "Allow HTTPS"
+    name = "ssh security group"
+    description = "enable enable ssh connection on port 22"
 
     dynamic "ingress" {
+        description = "ssh access"
         iterator = port
         for_each = var.ingressrules
         content {
-        from_port = port.value
-        to_port = port.value
+        from_port = 22
+        to_port = 22
         protocol = "TCP"
         cidr_blocks = ["0.0.0.0/0"]
         }
